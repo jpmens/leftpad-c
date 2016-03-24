4755b9644fe636552cbb07ab790958adc8616e82
Remi Rampin <remirampin@gmail.com>
2016-03-24 11:14:46 -0400

    Remove static function from header
    Fixes #1

--- leftpad.h
+++ leftpad.h
@@ -4,7 +4,4 @@
 char* left_pad(char* base_str, const unsigned int new_size);
 char* left_pad_str(char* base_str, const unsigned int new_size, const char pad_char);
 
-static
-char* do_left_pad(char* base_str, const unsigned int new_size, const char pad_char);
-
 #endif //LEFTPAD_C_LEFTPAD_H
